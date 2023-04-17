ba043d9c2138eb888935ad8b7434a4d5703972b0
adxxtya <andronaut9@gmail.com>
2023-04-17 21:56:49 +0530

    Integrated Google Analytics with PartyTown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+
+  integrations: [
+    tailwind(),
+    react(),
+    partytown({ config: { forward: ["dataLayer.push"], }, }),
+  ]
+  
 });
